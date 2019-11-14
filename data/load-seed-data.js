023d72527fc4ac589071ffe8f798dd962569db76
Eve Stockstill <evestockstillpdx@gmail.com>
2019-11-13 22:40:44 -0800

    setting up

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,7 @@
 const client = require('../lib/client');
 // import our seed data:
 const todos = require('./todos');
+const users = require('./users');
 
 run();
 
@@ -8,14 +9,29 @@ async function run() {
 
     try {
         await client.connect();
+        const savedUsers = await Promise.all (
+            users.map(async user => {
+                const result = await client.query(`
+                    INSERT INTO users (email)
+                    VALUES ($1)
+                    RETURNING *;
+                    `,
+                [user]);
+                return result.row[0];
+            })
+        );
 
         await Promise.all(
             todos.map(todo => {
+                const user = savedUsers.find(user => {
+                    return user.email === todo.user;
+                });
+                const userId = users.id;
                 return client.query(`
-                    INSERT INTO todos (task, complete)
-                    VALUES ($1, $2);
+                    INSERT INTO todos (user_id, email, hash, task, complete)
+                    VALUES ($1, $2, $3, $4, $5);
                 `,
-                [todo.task, todo.complete]);
+                [userId, user.email, users.hash, todo.task, todo.complete]);
             })
         );
 
--- server.js
+++ server.js
@@ -8,26 +8,69 @@ const morgan = require('morgan');
 const client = require('./lib/client.js');
 // Initiate database connection
 client.connect();
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client
+            .query(
+                `
+            SELECT id, email, hash 
+            FROM users
+            WHERE email = $1;
+        `,
+                [email]
+            )
+            .then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        console.log(user);
+        return client
+            .query(
+                `
+            INSERT into users (email, hash)
+            VALUES ($1, $2,)
+            RETURNING id, email;
+        `,
+                [user.email, hash]
+            )
+            .then(result => result.rows[0]);
+    }
+});
 
 // Application Setup
 const app = express();
 const PORT = process.env.PORT;
-app.use(morgan('dev')); // http logging
-app.use(cors()); // enable CORS request
-app.use(express.static('public')); // server files from /public folder
-app.use(express.json()); // enable reading incoming json data
+app.use(morgan('dev')); 
+app.use(cors()); 
+app.use(express.static('public')); 
+app.use(express.json()); 
+
+app.use('/api/auth', authRoutes);
+app.use('/api', ensureAuth);
 
 // API Routes
 
 // *** TODOS ***
+app.get('/api/test', (req, res) => {
+    res.json({
+        message: `the user's id is ${req.userId}`
+    });
+});
 
 
 app.get('/api/todos', async (req, res) => {
 
     try {
         const result = await client.query(`
-            SELECT * 
-            FROM todos
+            SELECT 
+            todo.id,
+            todo.task,
+            todo.type_id,
+            todo.task as type,
+            FROM todos t
+            JOIN types t
+            ON t.type_id = t.id
             
         `);
 
